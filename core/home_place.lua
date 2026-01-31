f00d56a1449693fff88accc8554d515dfc499f94
LinaPlusPlus <TenmaTengo@protonmail.com>
2026-01-30 19:06:28 -0500

    create home page

--- core/home_place.lua
+++ core/home_place.lua
@@ -4,26 +4,29 @@ function code_home()
     local i = 0;
     local j = 1;
 
-    writef("\nfunction at.home1()\n")
+    writef("\nfunction at.home1() if fresh then\n")
+    writef("mem.splash = \"Hello!\"\n")
     writef("ui_clear()\n");
     for name,action in pairs(apps) do
-        local x = i % 3;
-        local y = math.floor(i / 3);
+        local units = 3
+        local size = 3.5
+        local x = i % units;
+        local y = math.floor(i / units);
 
         
-        writef("ui_button(%q,%q,%s,%s,5,5)\n",action,name,x,y);
+        writef("ui_button(%q,%q,%s,%s,%s,1)\n",action,name,x*size,y,size);
 
         i = i + 1;
 
         if i >= 10 then
             i = 0;
             j = j + 1;
-            writef("\nend\n")
+            writef("\nend end\n")
             writef("\nfunction at.home%s()",j)
             writef("ui_clear()\n");
         end
 
     end
-    writef("\nend\n")
+    writef("\nend end\n")
 
 end
